40ed553c3edfd38f24ff8f8e0d9d6205bf9fc2c5
Paolo <keymo.kawaii@gmail.com>
2020-04-28 15:09:59 +0200

    Modif cookie suppr

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,42 +44,42 @@ module.exports = {
     //         '**/categories'
     //       ]
     //   }
-    },{
-      resolve: `gatsby-plugin-gdpr-cookies`,
-      options: {
-        googleAnalytics: {
-          trackingId: 'UA-162559347-1', // leave empty if you want to disable the tracker
-          cookieName: 'gatsby-gdpr-google-analytics', // default
-          anonymize: true // default
-        },
-    }
-  },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-162559347-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "DewanDev.com",
-      },
+  //   },{
+  //     resolve: `gatsby-plugin-gdpr-cookies`,
+  //     options: {
+  //       googleAnalytics: {
+  //         trackingId: 'UA-162559347-1', // leave empty if you want to disable the tracker
+  //         cookieName: 'gatsby-gdpr-google-analytics', // default
+  //         anonymize: true // default
+  //       },
+  //   }
+  // },
+  //   {
+  //     resolve: `gatsby-plugin-google-analytics`,
+  //     options: {
+  //       // The property ID; the tracking code won't be generated without it
+  //       trackingId: "UA-162559347-1",
+  //       // Defines where to place the tracking script - `true` in the head and `false` in the body
+  //       head: true,
+  //       // Setting this parameter is optional
+  //       anonymize: true,
+  //       // Setting this parameter is also optional
+  //       respectDNT: true,
+  //       // Avoids sending pageview hits from custom paths
+  //       exclude: ["/preview/**", "/do-not-track/me/too/"],
+  //       // Delays sending pageview hits on route update (in milliseconds)
+  //       pageTransitionDelay: 0,
+  //       // Enables Google Optimize using your container Id
+  //       optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+  //       // Enables Google Optimize Experiment ID
+  //       experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+  //       // Set Variation ID. 0 for original 1,2,3....
+  //       variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+  //       // Any additional optional fields
+  //       sampleRate: 5,
+  //       siteSpeedSampleRate: 10,
+  //       cookieDomain: "DewanDev.com",
+  //     },
     },
     {
       resolve: "gatsby-plugin-anchor-links",
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,14 @@ import './Layout.css';
 import Footer from '../pages/Footer/footer';
 // import GA from './../Tools/GoogleAnalytics';
 import Nav from './../pages/Nav/Nav';
-import PopupCookies from './../pages/Cookies/Cookies';
+// import PopupCookies from './../pages/Cookies/Cookies';
 
 export default ({children}) =>(
   <div>
     <Nav />
     {children}
     {/* <GA /> */}
-    <PopupCookies />
+    {/* <PopupCookies /> */}
     <Footer />
   </div>
 )
